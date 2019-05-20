5706d82f2799268a3084d12da033ee57d42be3bb
Noxalus <noxalus@gmail.com>
2019-05-20 22:06:45 +0200

    Create Unity project with LW render pipeline + create 2 fire shaders.

---
+++ ShaderTest/Assets/Scripts/FireMaterial.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class FireMaterial : MonoBehaviour
+{
+    public Image Image;
+    public Material Material;
+
+    void Start()
+    {
+        Image.material = Material;
+    }
+}
