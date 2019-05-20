9f4d6ef123c4db7206b722b31e1ad86ea52c37c8
Noxalus <noxalus@gmail.com>
2019-05-20 22:11:29 +0200

    Apply vector fire material on a UI image.

--- ShaderTest/Assets/Scripts/ChangeImageMaterial.cs
+++ ShaderTest/Assets/Scripts/ChangeImageMaterial.cs
@@ -1,7 +1,7 @@
 ﻿using UnityEngine;
 using UnityEngine.UI;
 
-public class FireMaterial : MonoBehaviour
+public class ChangeImageMaterial : MonoBehaviour
 {
     public Image Image;
     public Material Material;
